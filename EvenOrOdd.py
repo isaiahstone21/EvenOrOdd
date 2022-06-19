1e48e5e5abbec21da95527755bbf980c2ad8099b
Isaiah Stone <101003246+isaiahstone21@users.noreply.github.com>
2022-06-19 12:26:35 -0500

    Add files via upload

---
+++ EvenOrOdd.py
@@ -0,0 +1,11 @@
+def EvenOrOdd(num):
+    if num % 2 == 0:
+        print(str(num) + " is even")
+        return True
+    print(str(num) + " is odd")
+
+EvenOrOdd(0)
+EvenOrOdd(1)
+EvenOrOdd(2)
+EvenOrOdd(3)
+EvenOrOdd(4)
